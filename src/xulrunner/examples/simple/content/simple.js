5963d9158b48918be029045b37068feac69f834e
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-07-07 22:22:39 +1000

    Make the example run

--- src/xulrunner/examples/simple/content/simple.js
+++ src/xulrunner/examples/simple/content/simple.js
@@ -3,15 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+console.log("Hello world!");
+
 function onButtonClick() {
   var textbox = document.getElementById("textbox");
 
-  var contractid = (textbox.value % 2 == 0) ?
-      "@test.mozilla.org/simple-test;1?impl=js" :
-      "@test.mozilla.org/simple-test;1?impl=c++";
+  var contractid =
+    textbox.value % 2 == 0
+      ? "@test.mozilla.org/simple-test;1?impl=js"
+      : "@test.mozilla.org/simple-test;1?impl=c++";
 
-  var test = Components.classes[contractid].
-      createInstance(Components.interfaces.nsISimpleTest);
+  var test = Components.classes[contractid].createInstance(
+    Components.interfaces.nsISimpleTest
+  );
 
   textbox.value = test.add(textbox.value, 1);
 }
