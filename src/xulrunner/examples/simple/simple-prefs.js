408bcaac99a5789107995d39894ba37d1c763e17
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-07-08 19:19:55 +1000

    ✨ Use the new `AppLauncher`

--- src/xulrunner/examples/simple/simple-prefs.js
+++ src/xulrunner/examples/simple/simple-prefs.js
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("toolkit.defaultChromeURI", "chrome://simple/content/simple.html");
-pref("general.useragent.extra.simple", "SimpleApp/0.1");
+pref('app.content', 'chrome://simple/content/simple.html');
+pref('general.useragent.extra.simple', 'SimpleApp/0.1');
