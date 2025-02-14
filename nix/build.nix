11b27a1e43250959dc235998a00507b0f308c1eb
trickypr <git@trickypr.com>
2025-02-14 20:44:56 +1100

    feat: script to bundle gecko modules

--- nix/build.nix
+++ nix/build.nix
@@ -31,6 +31,26 @@ let
     libGL
   ];
   name = "quark-runtime";
+
+  link-modules = ''
+    # When run in the root of your quark-runtime application, it will link 
+    # all of the omni modules into your modules/ directory.
+    #
+    # This script (should) skip existing files
+
+    set -euo pipefail
+    IFS=$'\n\t'
+
+    target=$(pwd)/modules
+    mkdir -p $target
+
+    (
+      cd $quark/lib/omni/modules
+      find -type d -exec mkdir -p $target/{} \; 
+      find -type f -exec ln -s $(pwd)/{} $target/{} \; 
+      echo $target
+    )
+  '';
 in
 stdenv.mkDerivation {
   name = name;
@@ -38,8 +58,34 @@ stdenv.mkDerivation {
   builder = "${pkgs.bash}/bin/bash";
   buildInputs = [ unwrapped.gtk3 ];
   lib = lib.makeLibraryPath libs + ":" + lib.makeSearchPathOutput "lib" "lib64" libs;
+  outputs = [
+    "out"
+  ];
   args = [
     "-c"
-    "${pkgs.coreutils}/bin/mkdir -p $out/bin && ${pkgs.coreutils}/bin/ln -s ${unwrapped}/bin/${name} $out/bin/${name}"
+    ''
+      set -euo pipefail
+      IFS=$'\n\t'
+
+      ${pkgs.coreutils}/bin/mkdir -p $out/bin
+      ${pkgs.coreutils}/bin/mkdir -p $out/lib/omni
+
+      # Add the binary
+      ${pkgs.coreutils}/bin/ln -s ${unwrapped}/bin/${name} $out/bin/${name}
+
+      # Add development helpers 
+      ${pkgs.coreutils}/bin/cat >> link-modules << 'END'
+      ${link-modules}
+      END
+      echo -e "#! /usr/bin/env nix-shell\n#! nix-shell -i bash -p bash coreutils findutils\nquark='$out'" | ${pkgs.coreutils}/bin/cat - link-modules > $out/bin/link-modules
+      ${pkgs.coreutils}/bin/chmod +x $out/bin/link-modules
+
+      # Extract omni into lib
+      (
+        cd $out/lib/omni
+        ${pkgs.coreutils}/bin/cp ${unwrapped}/lib/quark-runtime/omni.ja .
+        ${pkgs.unzip}/bin/unzip ./omni.ja
+      )
+    ''
   ];
 }
