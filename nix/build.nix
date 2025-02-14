79859d8ae3d6f99dede76d51f6ecb8bee7df6719
trickypr <git@trickypr.com>
2025-02-14 16:46:17 +1100

    fix: include gtk3 in derivation inputs

--- nix/build.nix
+++ nix/build.nix
@@ -8,6 +8,7 @@ derivation rec {
   name = "quark-runtime";
   system = sys;
   builder = "${pkgs.bash}/bin/bash";
+  buildInputs = [ unwrapped.gtk3 ];
   args = [
     "-c"
     "${pkgs.coreutils}/bin/mkdir -p $out/bin && ${pkgs.coreutils}/bin/ln -s ${unwrapped}/bin/${name} $out/bin/${name}"
